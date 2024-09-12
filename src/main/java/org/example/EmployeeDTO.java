36366c2cf06e4481614fd45b8eb797ab8d1fea82
Luis Angel <luis.flores@durango.gob.mx>
2024-09-12 13:36:10 -0600

    Configuracion del pull de conexiones de hikari

---
+++ src/main/java/org/example/EmployeeDTO.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDTO {
+    private int id;
+    private String name;
+    private String email;
+    private int departmentId;
+    private String departmentName;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
+
+    private static void executeSqlScript(HikariDataSource dataSource) throws SQLException {
+        final String path = "src/main/resources/schema.sql";
+
+        try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()) {
+            FileInputStream fis = new FileInputStream(path);
+
+            String script = new Scanner(fis, StandardCharsets.UTF_8)
+                    .useDelimiter("\\A")
+                    .next();
+
+            statement.execute(script); // inicializando script en DB
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
